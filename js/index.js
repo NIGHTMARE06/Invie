7b3ed18875e830f8e3a8bc70d2a3017337adc21f
Alan Santiago <alanivan.s06@gmail.com>
2018-06-19 17:57:24 -0500

    Add media querys to an resize event

--- js/index.js
+++ js/index.js
@@ -1,13 +1,9 @@
 const $open = document.getElementById('btn-open')
 const $navMenu = document.getElementById('nav')
+const media = window.matchMedia('(max-width: 500px)')
 
-$open.addEventListener('touchstart', () =>  {
-  toggleMenu()
-})
-
-$open.addEventListener('click', () => {
-  toggleMenu()
-})
+mediaQuery()
+media.addListener(mediaQuery)
 
 function toggleMenu() {
   $navMenu.classList.toggle('active')
@@ -18,3 +14,14 @@ function toggleMenu() {
     $open.className = 'fas fa-bars btn-nav'
   }
 }
+
+function mediaQuery() {
+  if (media.matches) {
+    $open.addEventListener('touchstart', toggleMenu)
+    $open.addEventListener('click', toggleMenu)
+  } else {
+    $open.removeEventListener('touchstart', toggleMenu)
+    $open.removeEventListener('click', toggleMenu)
+  }
+}
+
